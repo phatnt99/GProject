65ab66d5ca8e62072c5e41c8363ff7bffc967da2
phatnguyen99 <phatnguyen240899@gmail.com>
2020-06-18 16:21:25 +0700

    refactor(model): create Auth model to retrieve Authenticatable

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use App\Traits\FreshTimestampTrait;
 use App\Traits\PrimaryKeyTrait;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
+
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Support\Facades\Hash;
 
